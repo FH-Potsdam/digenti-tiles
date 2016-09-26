3bc1fee71ae102c4c2705e1221e348516099694a
Jordi Tost <tost.jordi@gmail.com>
2016-09-26 18:09:55 +0200

    Example with filters

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ config.server.port = 10060; // https://wiki.uberspace.de/system:ports
 
 // Tile Settings
 config.tile = 'ndvi_gradient.mbtiles';
+// config.tile = 'colombia_fos.mbtiles';
 
 module.exports = config;
