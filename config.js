5cc5366313df09e33b398a6198ff8ce8cbdb4ffd
Jordi Tost <tost.jordi@gmail.com>
2016-09-29 16:59:01 +0200

    DIGENTI Tile Server (Uberspace)

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ var config = {};
 
 // Tile Server Settings
 config.server = {};
-config.server.port = 10060; // https://wiki.uberspace.de/system:ports
+config.server.host = 'localhost';
+// config.server.host = 'tostnik.deneb.uberspace.de/digenti-tiles';
+config.server.port = 61001; // https://wiki.uberspace.de/system:ports
 
 // Tile Settings
 config.tile = 'ndvi_gradient.mbtiles';
